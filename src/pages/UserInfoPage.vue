c50b95d24bbe86effe96936e34347df639b6f771
seojeong <sjytis14@naver.com>
2023-11-22 22:51:30 +0900

    [Feat] myPage 회원정보 수정, 사용자 계획 조회 분리

--- src/pages/UserInfoPage.vue
+++ src/pages/UserInfoPage.vue
@@ -1,9 +1,86 @@
 <script setup>
-import UserFormComponent from "src/components/user/UserFormComponent.vue";
+import TheHeadingNavbar from "src/components/common/TheHeadingNavbar.vue";
+import UserFormComponent from "../components/user/UserFormComponent.vue";
+import ContentsComponent from "src/components/main/ContentsComponent.vue";
+
+import { storeToRefs } from "pinia";
+import { useMemberStore } from "../stores/member";
+import { ref, onMounted } from "vue";
+import router from "src/router";
+
+const memberStore = useMemberStore();
+const { userInfo } = storeToRefs(memberStore);
+const modify = ref(false);
+
+/* scroll style */
+const thumbStyle = {
+  right: "4px",
+  borderRadius: "5px",
+  backgroundColor: "#ffc436",
+  width: "5px",
+  opacity: 0.75,
+};
+
+const barStyle = {
+  right: "2px",
+  borderRadius: "9px",
+  backgroundColor: "#ffc436",
+  width: "9px",
+  opacity: 0.2,
+};
+
+onMounted(() => {
+  if (!userInfo.value) {
+    router.push({ name: "main" });
+  }
+});
 </script>
 
 <template>
-  <UserFormComponent :type="'myPage'" />
+  <div class="container">
+    <TheHeadingNavbar />
+    <q-card
+      class="fixed-center fullscreen q-mt-lg"
+      style="width: 80%; height: 70vh"
+    >
+      <q-card-section>
+        <div class="card-header q-mt-md q-mb-xl">
+          <div class="text-h4">안녕하세요, {{ userInfo?.userName }}님</div>
+          <div>
+            <q-btn @click="modify = !modify">회원 정보 수정</q-btn>
+          </div>
+        </div>
+
+        <UserFormComponent :type="'myPage'" v-if="modify" />
+        <div v-else>
+          <div style="height: 100%; border: 1px solid black">
+            <q-scroll-area :thumb-style="thumbStyle" :bar-style="barStyle">
+              <ContentsComponent />
+            </q-scroll-area>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  height: 100vh;
+  text-align: center;
+}
+
+.q-scrollarea {
+  height: 40vh;
+  border-radius: 0.5rem;
+}
+.example-item {
+  height: 290px;
+  width: 290px;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
